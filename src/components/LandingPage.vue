139ac7a9fc79cf789b31165a98f2647e5e7e9f83
abiamrans <abiamrans@gmail.com>
2025-06-23 10:00:42 +0700

    fixing

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,6 +1,7 @@
 <template>
 	<link rel="stylesheet" href="/src/assets/LandingPage.css" />
-	<div class="container">
+	<section>	
+		<div class="container">
 		<header class="nav">
 			<img src="/src/assets/walogo.png" alt="Logo" class="logo" />
 			<nav class="navigation">
@@ -9,76 +10,100 @@
 			</nav>
 		</header>
 		<main class="content">
-			<h1>
-				FUN FIRST<br />
-				SKILL MORE!
-			</h1>
+			<img class="jargon" src="/src/assets/jargon1.png" alt="">
 			<div class="stats">
-				<div><strong>200+</strong> Peoples on WhatsApp</div>
-				<div><strong>420+</strong> Peoples on Instagram</div>
+				<img class="statspict" src="/src/assets/stats.png" alt="">
 			</div>
 		</main>
 	</div>
+</section>
+<section>
+	<div class="container2">
+		<h1>schedule</h1>
+		<div class="scheduleContainer">
+			<div class="info">
+    <div class="info__icon">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" height="24" fill="none"><path fill="#393a37" d="m12 1.5c-5.79844 0-10.5 4.70156-10.5 10.5 0 5.7984 4.70156 10.5 10.5 10.5 5.7984 0 10.5-4.7016 10.5-10.5 0-5.79844-4.7016-10.5-10.5-10.5zm.75 15.5625c0 .1031-.0844.1875-.1875.1875h-1.125c-.1031 0-.1875-.0844-.1875-.1875v-6.375c0-.1031.0844-.1875.1875-.1875h1.125c.1031 0 .1875.0844.1875.1875zm-.75-8.0625c-.2944-.00601-.5747-.12718-.7808-.3375-.206-.21032-.3215-.49305-.3215-.7875s.1155-.57718.3215-.7875c.2061-.21032.4864-.33149.7808-.3375.2944.00601.5747.12718.7808.3375.206.21032.3215.49305.3215.7875s-.1155.57718-.3215.7875c-.2061.21032-.4864.33149-.7808.3375z"></path></svg>
+    </div>
+    <div class="info__title">lorem ipsum dolor sit amet</div>
+    <div class="info__close"><svg height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="m15.8333 5.34166-1.175-1.175-4.6583 4.65834-4.65833-4.65834-1.175 1.175 4.65833 4.65834-4.65833 4.6583 1.175 1.175 4.65833-4.6583 4.6583 4.6583 1.175-1.175-4.6583-4.6583z" fill="#393a37"></path></svg></div>
+</div>
+<div class="info">
+    <div class="info__icon">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" height="24" fill="none"><path fill="#393a37" d="m12 1.5c-5.79844 0-10.5 4.70156-10.5 10.5 0 5.7984 4.70156 10.5 10.5 10.5 5.7984 0 10.5-4.7016 10.5-10.5 0-5.79844-4.7016-10.5-10.5-10.5zm.75 15.5625c0 .1031-.0844.1875-.1875.1875h-1.125c-.1031 0-.1875-.0844-.1875-.1875v-6.375c0-.1031.0844-.1875.1875-.1875h1.125c.1031 0 .1875.0844.1875.1875zm-.75-8.0625c-.2944-.00601-.5747-.12718-.7808-.3375-.206-.21032-.3215-.49305-.3215-.7875s.1155-.57718.3215-.7875c.2061-.21032.4864-.33149.7808-.3375.2944.00601.5747.12718.7808.3375.206.21032.3215.49305.3215.7875s-.1155.57718-.3215.7875c-.2061.21032-.4864.33149-.7808.3375z"></path></svg>
+    </div>
+    <div class="info__title">lorem ipsum dolor sit amet</div>
+    <div class="info__close"><svg height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="m15.8333 5.34166-1.175-1.175-4.6583 4.65834-4.65833-4.65834-1.175 1.175 4.65833 4.65834-4.65833 4.6583 1.175 1.175 4.65833-4.6583 4.6583 4.6583 1.175-1.175-4.6583-4.6583z" fill="#393a37"></path></svg></div>
+</div>
+<div class="info">
+    <div class="info__icon">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" height="24" fill="none"><path fill="#393a37" d="m12 1.5c-5.79844 0-10.5 4.70156-10.5 10.5 0 5.7984 4.70156 10.5 10.5 10.5 5.7984 0 10.5-4.7016 10.5-10.5 0-5.79844-4.7016-10.5-10.5-10.5zm.75 15.5625c0 .1031-.0844.1875-.1875.1875h-1.125c-.1031 0-.1875-.0844-.1875-.1875v-6.375c0-.1031.0844-.1875.1875-.1875h1.125c.1031 0 .1875.0844.1875.1875zm-.75-8.0625c-.2944-.00601-.5747-.12718-.7808-.3375-.206-.21032-.3215-.49305-.3215-.7875s.1155-.57718.3215-.7875c.2061-.21032.4864-.33149.7808-.3375.2944.00601.5747.12718.7808.3375.206.21032.3215.49305.3215.7875s-.1155.57718-.3215.7875c-.2061.21032-.4864.33149-.7808.3375z"></path></svg>
+    </div>
+    <div class="info__title">lorem ipsum dolor sit amet</div>
+    <div class="info__close"><svg height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="m15.8333 5.34166-1.175-1.175-4.6583 4.65834-4.65833-4.65834-1.175 1.175 4.65833 4.65834-4.65833 4.6583 1.175 1.175 4.65833-4.6583 4.6583 4.6583 1.175-1.175-4.6583-4.6583z" fill="#393a37"></path></svg></div>
+</div>
+		</div>
+
+	</div>
+</section>
+<footer class="footer">
+  <div class="footer-top">
+    <div class="footer-section social">
+      <h4>FOLLOW US</h4>
+      <div class="social-icons">
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-google-plus-g"></i></a>
+        <a href="#"><i class="fab fa-pinterest-p"></i></a>
+        <a href="#"><i class="fab fa-behance"></i></a>
+      </div>
+    </div>
+    <div class="footer-section">
+      <h4>SHOP</h4>
+      <ul>
+        <li><a href="#">New In</a></li>
+        <li><a href="#">Shoes</a></li>
+        <li><a href="#">Bags</a></li>
+        <li><a href="#">Shirts</a></li>
+        <li><a href="#">Jackets</a></li>
+        <li><a href="#">Accessories</a></li>
+      </ul>
+    </div>
+    <div class="footer-section">
+      <h4>INFORMATION</h4>
+      <ul>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Customers</a></li>
+        <li><a href="#">Service</a></li>
+        <li><a href="#">Collection</a></li>
+        <li><a href="#">Sellers</a></li>
+      </ul>
+    </div>
+    <div class="footer-section">
+      <h4>PRESS</h4>
+      <ul>
+        <li><a href="#">Press Releases</a></li>
+        <li><a href="#">Awards</a></li>
+        <li><a href="#">Testimonials</a></li>
+        <li><a href="#">Timeline</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <a href="#">HOME</a>
+    <a href="#">EXPLORE</a>
+    <a href="#">WORKS</a>
+    <a href="#">SHOP</a>
+    <a href="#">ABOUT US</a>
+    <a href="#">CONTACT</a>
+  </div>
+</footer>
 </template>
 
 <style>
-.container {
-	position: relative;
-	min-height: 100vh;
-	background-image: url('/src/assets/background.jpg');
-	background-size: cover;
-	background-position: center;
-	color: #e7e8cd;
-	font-family: Arial, sans-serif;
-}
 
-.nav {
-	position: absolute;
-	top: 20px;
-	left: 20px;
-	display: flex;
-	align-items: center;
-	gap: 20px;
-}
 
-.nav .logo {
-	width: 40px;
-	height: 40px;
-}
 
-.nav a {
-	color: #e7e8cd;
-	text-decoration: none;
-	font-weight: bold;
-	text-transform: uppercase;
-	font-size: 14px;
-}
-
-.nav a:hover {
-	text-decoration: underline;
-}
-
-.content {
-	height: 100vh;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	padding-left: 60px;
-}
-
-.content h1 {
-	font-size: 64px;
-	line-height: 1.2;
-	font-weight: 800;
-	margin-bottom: 30px;
-}
-
-.stats {
-	font-size: 20px;
-	font-weight: bold;
-}
-
-.stats div {
-	margin-bottom: 10px;
-}
 </style>
